cd6611a95679aec468eb81c6a0714d138f833ebe
gruposeven <marcospaulo.oliveira@gruposeven.org>
2018-04-28 01:32:49 -0300

    Inicio da Criação do template do sistema e sua responsividade

---
+++ script/script-v-s-1-0.js
@@ -0,0 +1,9 @@
+function togglemenu(){
+    var abrirmenu = document.getElementById("abrirmenu");
+    if(abrirmenu.style.display == "none" || abrirmenu.style.display ==""){
+        abrirmenu.style.display="block";
+        
+    }else{
+        abrirmenu.style.display="none";
+    }
+}
---
+++ sistemaTopo1.php
@@ -0,0 +1,124 @@
+<html>
+	<head>
+		<title>Grupo Seven Brasil</title>
+		<meta charset="UTF-8">
+		<meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+		
+		<link rel="stylesheet" type="text/css" href="css/style-v-s-1-0.css">
+		<link rel="stylesheet" type="text/css" href="biblioteca/normalize.css">
+		<link rel="shortcut icon" type="text/css" href="imagens/sistema.ico">
+
+		<script type="text/javascript" src="script/script-v-s-1-0.js"></script>
+		<script type="text/javascript" src="biblioteca/jquery-3.2.1.min.js"></script>
+
+<!--Código Google AdSenese-->
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8856742325034767",
+    enable_page_level_ads: true
+  });
+</script>
+<!--Fim do código Google AdSenese-->
+
+		<body>
+
+
+			<!--Topo 1 Inicio-->
+			<div class="topo1">
+				<div class="topo1Int">
+					<div class="topo1IntEsquerdo">
+						<ul>
+							<a href="./" target="_blanck"><li>Empresas</li></a>
+							<a href="./" target="_blanck"><li>Condomínios</li></a>
+							<a href="./" target="_blanck"><li>Bancos</li></a>
+							<a href="./" target="_blanck"><li>Governo</li></a>
+						</ul>	
+					</div>
+					<div class="topo1IntDireito">
+						<a href="./" target="_blanck"><img src="imagens/youtube.png" /></a>
+						<a href="./" target="_blanck"><img src="imagens/twitter.png" /></a>
+						<a href="./" target="_blanck"><img src="imagens/rss.png" /></a>
+						<a href="./" target="_blanck"><img src="imagens/googleplus.png" /></a>
+						<a href="./" target="_blanck"><img src="imagens/facebook.png" /></a>
+						<a href="./" target="_blanck"><img src="imagens/email.png" /></a>
+					</div>
+				</div>
+			</div>
+			<!--Topo 1 Fim-->
+
+			<!--Topo 2 Inicio-->
+			
+			<div class="topo2">
+				<div class="topo2Int">
+					<div class="topo2Logo">
+						<img src="imagens/logog7b.png">
+					</div>
+					<div class="pesquisaEmenu">
+						<div class="pesquisa">
+							<div class="pesquisaInt">
+<script>
+(function() {
+var cx = '014863200606814594070:7ufq1pde_9s';
+var gcse = document.createElement('script');
+gcse.type = 'text/javascript';
+gcse.async = true;
+gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(gcse, s);
+})();
+</script>
+<gcse:search></gcse:search>
+							</div>
+						</div> 	
+						<div class="menu">
+							<img src="imagens/mobilemenu.png" border="0" width="80" heigth="30" class="menumobile" onclick="togglemenu()">
+							<ul id="abrirmenu">
+
+								<li><a href="./" target="_blanck">Painel</a></li>
+
+								<li>
+									<a href="./" target="_blanck">Diretoria</a>
+									<img src="imagens/setasubmenu.png" border="0" width="10">
+
+									<div class="submenu">	
+		                                <a href="./"><div class="submenuItem">Submenu1</div></a>
+		                                <a href="./"><div class="submenuItem">Submenu2</div></a>
+		                                <a href="./"><div class="submenuItem">Submenu3</div></a>
+		                                <a href="./"><div class="submenuItem">Submenu4</div></a>
+
+									</div>
+								</li>
+								<li><a href="./" target="_blanck">Contabilidade</a></li>
+								<li><a href="./" target="_blanck">Contratos</a></li>
+								<li><a href="./" target="_blanck">Recursos Humanos</a></li>
+								<li><a href="./" target="_blanck">Coordenação</a></li>
+
+							</ul>	
+						</div>
+					</div>
+				</div>
+			</div>	
+<!--Topo 2 Fim-->
+			
+			<div class="corpo">
+				<div class="corpoInt">
+					<div class="corpoInt1">
+
+					</div>
+					<div class="corpoInt2">
+
+					</div>
+					<div class="corpoInt3">
+
+					</div>
+					<div class="corpoInt4">
+
+					</div>
+				</div>
+			</div>
+
+		</body>
+	</head>
+</html>
