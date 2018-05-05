9e394124c50aec9a48ac8d78e85bcdee75113dee
gruposeven <marcospaulo.oliveira@gruposeven.org>
2018-05-05 13:05:49 -0300

    Criação versão 1.1 do sistema com novo layout da pagina de autenticação

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<title>Grupo Seven Brasil</title>
+		<meta charset="UTF-8">
+		<meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+		
+		<link rel="stylesheet" type="text/css" href="css/style-v1-2.css">
+		<link rel="stylesheet" type="text/css" href="biblioteca/normalize.css">
+		<link rel="shortcut icon" type="text/css" href="imagens/sistema.ico">
+
+		<script type="text/javascript" src="script/script-v1-2
+		.js"></script>
+		<script type="text/javascript" src="biblioteca/jquery-3.2.1.min.js"></script>
+		<body>
+			
+			<div class="body_autenticacao">
+				
+				<div class="body_autenticacao_inicio">
+						<img src="imagens/logog7.png">
+				</div>
+
+				<div class="body_autenticacao_meio">
+					<form metdh="POST" onsubmit="return validar()">
+						<input type"text" name="usuario" class="autenticacao_campo"><br>
+						<input type"password" name="senha" class="autenticacao_campo"><br>
+					
+						<input type="submit" class="autenticacao_submit" value="Autenticar">
+					</form>
+				</div>
+				<div class="body_autenticacao_fim">
+					<div class="body_autenticacao_fim_direito">
+						<img src="imagens/assistente1.png">
+					</div>
+					<div class= "body_autenticacao_fim_esquerdo">
+						<h4> Sistema Grupo Seven Brasil</h4>
+						Serei sua assistente virtual e irei te auxiliar em que precisar... 
+					</div>
+				</div>
+
+			</div>
+			<div class="body_opcoes">
+				<button class="body_opcoes_novo">Cadastre-se
+				</button>
+				<button class="body_opcoes_esqueci">Esqueci Senha
+				</button>
+
+			</div>			
+		</body>
+	</head>
+
+</html>
--- script/script-v-s-1-0.js
+++
@@ -1,9 +0,0 @@
-function togglemenu(){
-    var abrirmenu = document.getElementById("abrirmenu");
-    if(abrirmenu.style.display == "none" || abrirmenu.style.display ==""){
-        abrirmenu.style.display="block";
-        
-    }else{
-        abrirmenu.style.display="none";
-    }
-}
