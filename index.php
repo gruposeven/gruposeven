695d0e7b7d1a827b139f411d9d5d0a1d21c0026f
gruposeven <marcospaulo.oliveira@gruposeven.org>
2018-05-05 13:13:08 -0300

    modificação no head com os nomes de arquivo. css e .js

--- index.php
+++ index.php
@@ -4,12 +4,11 @@
 		<meta charset="UTF-8">
 		<meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
 		
-		<link rel="stylesheet" type="text/css" href="css/style-v1-2.css">
+		<link rel="stylesheet" type="text/css" href="css/style-v1-1.css">
 		<link rel="stylesheet" type="text/css" href="biblioteca/normalize.css">
 		<link rel="shortcut icon" type="text/css" href="imagens/sistema.ico">
 
-		<script type="text/javascript" src="script/script-v1-2
-		.js"></script>
+		<script type="text/javascript" src="script/script-v1-1.js"></script>
 		<script type="text/javascript" src="biblioteca/jquery-3.2.1.min.js"></script>
 		<body>
 			
