e9531463c07a2e429055259e132d11b4058e7aad
ADM-TI <ADM-TI@BASE-DIR-GERAL>
2019-03-13 01:40:59 -0300

    configuração do GitHub no Netbens e inclusão de biblioteas java e css

---
+++ sistema/menu_nav.php
@@ -0,0 +1,23 @@
+				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+					<a href="./" class="navbar-brand" id="titulo">Sistema Grupo Seven Brasil</a>
+					<button class="navbar-toggler" data-toggle="collapse" data-target="#navemenu">
+						<span class="navbar-toggler-icon">
+						</span>
+					</button>
+
+					<div class="navbar-collapse collapse" id="navemenu">
+						<div class="navbar-nav">
+							<a href="../index.php" class="nav-item nav-link">Home</a>
+							<a href="" class="nav-item nav-link">Diretoria</a>
+							<a href="" class="nav-item nav-link">Contratos</a>
+							<a href="" class="nav-item nav-link">Contabilidade</a>
+							<a href="" class="nav-item nav-link">Recursos Humanos</a>
+							<a href="" class="nav-item nav-link">Coordenação</a>
+							<a href="" class="nav-item nav-link">Configurações</a>
+							<a href="" class="nav-item nav-link">Perfil</a>
+
+							
+
+						</div>
+					</div> 
+				</nav>
--- sistema/sistema.php
+++ sistema/sistema.php
@@ -10,12 +10,14 @@
 		<link rel="shortcut icon" type="text/css" href="../imagens/sistema.ico">
 <!--Bibliotecas-->
 		<link rel="stylesheet" type="text/css" href="../biblioteca/normalize.css">
+		<link rel="stylesheet" type="text/css" href="../biblioteca/bootstrap.min.css">
 		<script type="text/javascript" src="../biblioteca/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="../biblioteca/bootstrap.bundle.min.js"></script>
+
 <!--Estilo-->
-		<link rel="stylesheet" type="text/css" href="../css/style-v1-1.css">
+		<link rel="stylesheet" type="text/css" href="css-sistema/style-sistema.css">
 <!--javascript-->
-		<script type="text/javascript" src="../script/script-v1-1.js"></script>
-		<script type="text/javascript" src="../script/verificador-v1-1.js"></script>
+		<script type="text/javascript" src="script-sistema/script-sistema.js"></script>
 <?php
 session_start();
 
@@ -28,3 +30,14 @@ if(isset($_SESSION['usuario']) && ($_SESSION['usuario'] != "")){
 }else{
 	header("Location:../index.php");
 }
+?>
+
+</head>
+<body>
+<!-- Navegação-->
+	<div class="container">
+		<?php
+		require "menu_nav.php"
+		?>
+	</div>
+</body>
